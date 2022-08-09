a55ca7d8f332c828afa3f57ef9f3de395f4b289d
Pedro Henrique Custódio Filho <pedroo.custodio@hotmail.com>
2022-08-09 20:40:31 -0300

    Versionamento atividade 1 da UC8

---
+++ atividade_remoto1.js
@@ -0,0 +1,35 @@
+// inicio cadastro por data do evento
+const DataDoEvento = ["1"]
+console.log("inicio")
+
+
+// campo de inserção da data do evento
+let dataevento = 1
+if (dataevento = 1) {console.log("ok, pode cadastrar")}
+else {console.log("data do evento inválida")}
+
+// processamento para próxima etapa após inserção da data do evento
+console.log("processando")
+
+//inicio cadastro por idade do participante
+const IdadeDoParticipante = ["18"]
+
+//campo de inserção da idade do participante
+let CadastroIdade = 18
+if (CadastroIdade < 18) {console.log("Cadastro não permitido, idade menor que 18 anos")}
+else {console.log("Cadastro permitido")}
+
+// processamento para próxima etapa após inserção da data do evento
+console.log("processando")
+
+//inicio do cadastro por número de participantes
+const ListaDeParticipantes = ["100"]
+
+//campo de verificação do número de participantes do evento
+let CadastroParticipante = 99
+if (CadastroParticipante < 100) {console.log("Cadastro permitido")}
+else {console.log("Cadastro não permitido, limite de participantes excedido")}
+
+//processo de finalização do cadastro
+console.log("Inscrição realizada!")
+
